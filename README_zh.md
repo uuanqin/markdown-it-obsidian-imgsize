0c8d24f10b3b1a891142d7caf60653c342f3f7fe
wuanqin <uuanqin@qq.com>
2023-10-20 12:46:40 +0800

    first publish

--- README_zh.md
+++ README_zh.md
@@ -1,31 +1,31 @@
 # markdown-it-obsidian-imgsize
 
-language: English | [中文](./READEME_zh.md)
+语言: [English](./README.md) | 中文
 
-A markdown-it plugin, adding width (and height) attributes to image elements.
+为图像元素增加width属性（以及height属性）的markdown-it插件。
 
-According to [the syntax of external images in Obsidian](https://help.obsidian.md/Editing+and+formatting/Basic+formatting+syntax#External+images)
-, it transforms
+根据 [Obsidian的图片大小设置语法](https://help.obsidian.md/Editing+and+formatting/Basic+formatting+syntax#External+images)
+，插件将以下内容
 ```markdown
 ![A cat|100](cat.jpg)
 ![A cat|100x200](cat.jpg)
 ```
-to
+转化为
 ```html
 <p><img src="cat.jpg" alt="A cat" width="100px"></p>
 <p><img src="cat.jpg" alt="A cat" width="100px" height="200px"></p>
 ```
 
-More cases are in [examples.txt](./test/examples.txt). Different from [markdown-it-renderer-image](https://www.npmjs.com/package/@peaceroad/markdown-it-renderer-image), 
-this plugin does not use the Internet when processing online images.
+更多例子详见[examples.txt](./test/examples.txt)。与插件[markdown-it-renderer-image](https://www.npmjs.com/package/@peaceroad/markdown-it-renderer-image)不同的是，
+本插件在处理网络图片时无需联网。
 
-## Install
+## 安装
 
 ```bash
 npm i markdown-it-obsidian-imgsize --save
 ```
 
-## Use
+## 使用
 
 ```js
 const md = require('markdown-it')();
@@ -35,16 +35,16 @@ md.use(mdImgSizePlg);
 const content = '![A cat|100](cat.jpg)';
 console.log(md.render(content));
 // <p><img src="cat.jpg" alt="A cat" width="100px"></p>
-// See './test/examples.txt' for more examples
+// 更多例子详见 './test/examples.txt'
 ```
 
-## Related Plugins
+## 相关插件
 
 [markdown-it-renderer-image](https://www.npmjs.com/package/@peaceroad/markdown-it-renderer-image)
 
 [markdown-it-obsidian-images](https://www.npmjs.com/package/markdown-it-obsidian-images)
 
-## License
+## 许可证
 MIT
 
 
--- index.js
+++ index.js
@@ -1,88 +1,5 @@
-const path = require('path');
-const fetch = require("sync-fetch");
-const sizeOf = require('image-size');
-
 module.exports = function renderer_image_plugin(md, option) {
-  /*const option = {
-    scaleSuffix: false,
-    mdPath: '',
-    lazyLoad: false,
-    resize: false,
-    asyncDecode: false,
-  };*/
-
-  function setImgSize(token, img, imgData) {
-    if (!imgData) return token;
-
-    let w = imgData.width;
-    let h = imgData.height;
-    //console.log('w: ' + w + ', h: ' + h);
-
-    const imgName = path.basename(img, path.extname(img));
-    if (option.scaleSuffix) {
-      const reg = /[@._-]([0-9]+)(x|dpi|ppi)$/;
-      const rs = imgName.match(reg);
-      if (rs) {
-        if (rs[2] === 'x') {
-          w = Math.round(w / rs[1]);
-          h = Math.round(h / rs[1]);
-        }
-        if (/(dpi|ppi)/.test(rs[2])) {
-          w = Math.round(w * 96 / rs[1]);
-          h = Math.round(h * 96 / rs[1]);
-        }
-      }
-    }
-    const imgTitle = token.attrGet('title');
-    if (imgTitle && option.resize) {
-      const resizeReg = /(?:(?:(?:大きさ|サイズ)の?変更|リサイズ|resize(?:d to)?)? *[:：]? *([0-9]+)([%％]|px)|([0-9]+)([%％]|px)に(?:(?:大きさ|サイズ)を?変更|リサイズ))/i;
-      const hasResizeSetting = imgTitle.match(resizeReg);
-      if(hasResizeSetting) {
-        const resizeValue = +hasResizeSetting[1];
-        const resizeUnit = hasResizeSetting[2];
-        //console.log('w: ' + w + ', h: ' + h);
-        if (resizeUnit.match(/[%％]/)) {
-          w = Math.round(w * resizeValue / 100);
-          h = Math.round(h * resizeValue / 100);
-        }
-        if (resizeUnit.match(/px/)) {
-          const bw = w;
-          w = Math.round(resizeValue);
-          h = Math.round(h * resizeValue / bw);
-        }
-      }
-    }
-    //console.log('w: ' + w + ', h: ' + h);
-    token.attrJoin('width', w);
-    token.attrJoin('height', h);
-    return token;
-  }
-
-  function addAsyncDecode(imgCont) {
-    imgCont = imgCont.replace(/( *?\/)?>$/, ' decoding="async"$1>');
-    return imgCont;
-  }
-
-  function addLazyLoad(imgCont) {
-    imgCont = imgCont.replace(/( *?\/)?>$/, ' loading="lazy"$1>');
-    return imgCont;
-  }
-
-  function setLocalImgSrc(imgSrc, option, env) {
-    let img = '';
-    if (option.mdPath) {
-      img = path.dirname(option.mdPath);
-    } else {
-      if (env !== undefined) {
-        if (env.mdPath) {
-          img = path.dirname(env.mdPath);
-        }
-      }
-    }
-    img += path.sep + imgSrc;
-    img = decodeURI(img);
-    return img;
-  }
+  /*const option = { no option here };*/
 
   md.renderer.rules['image'] = function (tokens, idx, options, env, slf) {
     let endTagCont = '>';
@@ -90,7 +7,12 @@ module.exports = function renderer_image_plugin(md, option) {
       endTagCont = ' />';
     }
     const token = tokens[idx];
-    let imgAlt = md.utils.escapeHtml(token.content);
+
+    // See https://help.obsidian.md/Editing+and+formatting/Basic+formatting+syntax#External+images
+    var obsidian_imgsize_RE = /\| *([0-9]+) *(x *([0-9]+))? *$/;
+    let imgAlt_original = md.utils.escapeHtml(token.content)
+
+    let imgAlt = imgAlt_original.replace(obsidian_imgsize_RE,"");
     let imgSrc = md.utils.escapeHtml(token.attrGet('src'));
     let imgTitle = md.utils.escapeHtml(token.attrGet('title'));
     let imgCont = '<img src="' + decodeURI(imgSrc) + '"' + endTagCont;
@@ -98,42 +20,24 @@ module.exports = function renderer_image_plugin(md, option) {
     if (imgTitle) {
       imgCont = imgCont.replace(/( *?\/)?>$/, ' title="' + imgTitle + '"$1>');
     }
-    if (option.asyncDecode) {
-      imgCont = addAsyncDecode(imgCont);
-    }
-    if (option.lazyLoad) {
-      imgCont = addLazyLoad(imgCont);
-    }
 
-    let isNotLocal = /^https?:\/\//.test(imgSrc);
-    let imgData = {};
-
-    if (isNotLocal) {
-      try {
-        const response = fetch(imgSrc);
-        const buffer = response.buffer();
-        imgData = sizeOf(buffer);
-      } catch {
-        console.error('[renderder-image]No image: ' + imgSrc);
-      }
-      if (imgData.width !== undefined) {
-        setImgSize(token, imgSrc, imgData);
-        imgCont = imgCont.replace(/( *?\/)?>$/, ' width="' + token.attrGet('width') + '" height="' + token.attrGet('height') + '"$1>');
-      }
-
-    } else {
-      imgSrc = setLocalImgSrc(imgSrc, option, env)
-      try {
-        imgData = sizeOf(imgSrc);
-      } catch {
-        console.error('[renderder-image]No image: ' + imgSrc);
-      }
-      if (imgData.width !== undefined) {
-        setImgSize(token, imgSrc, imgData);
-        imgCont = imgCont.replace(/( *?\/)?>$/, ' width="' + token.attrGet('width') + '" height="' + token.attrGet('height') + '"$1>');
+    let matchObj = imgAlt_original.match(obsidian_imgsize_RE)
+    if(matchObj){
+      let width,height;
+      if(matchObj[2]){
+        // e.g. ![img|100x200](/path/to/img)
+        width = parseInt(matchObj[1]);
+        height = parseInt(matchObj[3])
+        token.attrJoin('width', width);
+        token.attrJoin('height', height);
+        imgCont = imgCont.replace(/( *?\/)?>$/, ` width="${width}px" height="${height}px"` + '$1>');
+      }else{
+        // e.g. ![img|100](/path/to/img)
+        width = parseInt(matchObj[1]);
+        token.attrJoin('width', width);
+        imgCont = imgCont.replace(/( *?\/)?>$/, ` width="${width}px"` + '$1>');
       }
     }
-
     return imgCont;
   }
 };
